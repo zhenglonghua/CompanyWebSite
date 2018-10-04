81ef8c7c576df317e80829aaf7ba68340345e3ca
zhenglonghua <zhenglonghuafred@126.com>
2018-10-04 16:39:20 +0800

    add
    创建项目路径

---
+++ source/PythonCompany/src/main/java/com/python/PythonCompanyApplication.java
@@ -0,0 +1,12 @@
+package com.python;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PythonCompanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PythonCompanyApplication.class, args);
+	}
+}
