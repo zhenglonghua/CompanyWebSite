ffb12b75d0f5f2f44bd4dab2cbf4c591c8c822bb
zhenglonghua <zhenglonghuafred@126.com>
2018-10-14 16:31:52 +0800

    add  ProducerAndComsumerPattern
    ProducerAndComsumerPattern

--- source/PythonCompany/src/main/java/com/python/PythonCompanyApplication.java
+++ source/PythonCompany/src/main/java/com/python/PythonCompanyApplication.java
@@ -2,11 +2,18 @@ package com.python;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class PythonCompanyApplication {
+public class PythonCompanyApplication implements WebMvcConfigurer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(PythonCompanyApplication.class, args);
 	}
+	
+	 @Override
+	 public void addViewControllers(ViewControllerRegistry registry) {
+	      registry.addRedirectViewController("/", "/home");
+	 }
 }
